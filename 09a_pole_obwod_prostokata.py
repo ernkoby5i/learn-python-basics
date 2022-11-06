eac5878fc5a4b1900256c735285d98efe5dfe118
ernkoby5i <ernkoby5i@gmail.com>
2022-11-06 09:13:19 +0100

    09 Funkcje 09a_zadanie_1_pole_obwod_prostokata.py 09b_zadanie_2_liczba_pierwszza.py

---
+++ 09a_pole_obwod_prostokata.py
@@ -0,0 +1,3 @@
+# Zadanie 1
+# Step 1: napisz funkcje obwod_prostokaota(a,b)
+# Step 2: napisz funkcje pole_prostokata(a,b)
---
+++ 09b_zadaie_2_liczba_pierwszza.py
@@ -0,0 +1,6 @@
+# Zadaie 2: Liczby pierwsze
+# sprawdz czy liczna n jest liczba piwersza
+# Step 1. sprawdz czy dzieli sie przez liczby od 2, n-1 jezeli tak to nie jest
+#         uzyj for i range
+# Step 2. zamien kod w funkcje ktora
+# Step 3. znajdz liczby pierwsze 2 przedziale 1 .. 100
