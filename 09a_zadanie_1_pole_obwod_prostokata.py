25d7b7be7ae45df30c84dea4f2123b221f91a5ca
koby5i <bobkoby5i@xdeq.com>
2022-11-06 16:25:17 +0100

    Tzrojkat obwod, pole, wzor Herona

--- 09a_zadanie_1_pole_obwod_prostokata.py
+++ 09a_zadanie_1_pole_obwod_prostokata.py
@@ -1,9 +1,37 @@
 # Zadanie 1
-# Step 1: napisz funkcje obwod_prostokaota(a,b)
+# Step 1: napisz funkcje obwod_prostokata(a,b)
 # Step 2: napisz funkcje pole_prostokata(a,b)
 
+import math
+
+
 def obwod_prostokonta(a, b):
+    print(f"obliczam obwód a={a} b={b}")
     obwod = a * 2 + b * 2
     return obwod
 
+
+def pole_prostokonta(a, b):
+    print("obliczam pole a=" + str(a) + " b=" + str(b))
+    pole = a * b
+    return pole
+
+
+def obwod_trojkta(a, b, c):
+    print(f"obliczam obwód a={a} b={b} c={c}")
+    obwod = a + b + c
+    return obwod
+
+# Wzor Herona
+def pole_trojkata(a, b, c):
+    print("obliczam pole a=" + str(a) + " b=" + str(b) + " c=" + str(c))
+    p = 1 / 2 * (a + b + c)
+    pole = math.sqrt(p * (p - a) * (p - b) * (p - c))
+
+    return pole
+
+
 print("Obwod prostokata : " + str(obwod_prostokonta(5, 3)))
+print("pole prostokata : " + str(pole_prostokonta(5, 3)))
+print("Obwod trójkata : " + str(obwod_trojkta(5, 3, 4)))
+print("pole trójkonta : " + str(pole_trojkata(4, 5, 7)))
