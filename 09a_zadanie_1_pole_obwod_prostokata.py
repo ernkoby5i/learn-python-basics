c43d0edaea4a8ea9555a18efb3282f7315c4ba93
ernkoby5i <ernkoby5i@gmail.com>
2022-11-06 14:34:55 +0100

    poprawiam ortografie

--- 09a_zadanie_1_pole_obwod_prostokata.py
+++ 09a_zadanie_1_pole_obwod_prostokata.py
@@ -2,8 +2,8 @@
 # Step 1: napisz funkcje obwod_prostokaota(a,b)
 # Step 2: napisz funkcje pole_prostokata(a,b)
 
-def obwud_prostokonta(a, b):
+def obwod_prostokonta(a, b):
     obwod = a * 2 + b * 2
     return obwod
 
-print(obwud_prostokonta(5, 3))
+print("Obwod prostokata : " + str(obwod_prostokonta(5, 3)))
