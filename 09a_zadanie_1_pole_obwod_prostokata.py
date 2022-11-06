d5dfe8272bac3c4773d7659a61f4b0bc17b6a3ef
koby5i <bobkoby5i@xdeq.com>
2022-11-06 14:32:29 +0100

    obwod prostokata

--- 09a_zadanie_1_pole_obwod_prostokata.py
+++ 09a_zadanie_1_pole_obwod_prostokata.py
@@ -1,3 +1,9 @@
 # Zadanie 1
 # Step 1: napisz funkcje obwod_prostokaota(a,b)
 # Step 2: napisz funkcje pole_prostokata(a,b)
+
+def obwud_prostokonta(a, b):
+    obwod = a * 2 + b * 2
+    return obwod
+
+print(obwud_prostokonta(5, 3))
