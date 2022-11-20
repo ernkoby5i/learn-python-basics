2933bc9892a208be92819e4afce07e680d7b959c
ernkoby5i <ernkoby5i@gmail.com>
2022-11-20 19:53:17 +0100

    11a_zadanie_3_tabliczka_mnozenia init

---
+++ 11a_zadanie_3_tabliczka_mnozenia.py
@@ -0,0 +1,37 @@
+# program do nauki tabliczki monozenia
+# Phase 1:
+#  skorzystaj z random ktora miales w module math
+#  a = wylosuj liczbe z zakresu 2..10
+#  b = wylosuj liczbe z zakresu 2..10
+#  wypisz przy pomocy print obie liczby
+#  wczytaj liczbe odpowiedz uzywajac input
+#  sprawdz czy a x b == odpowiedz przy pomocy if
+#  jezeli prawqda wupisz BEAVO a jezeli falsz wypisz SLABO :(
+
+# Phase 2:
+#  zamien powyzszy kod na funkcje mnozenie_odpowiedz_prawidlowa()
+#  def zadanie_rozwiazane_prawidlowo():
+#  funkcja zwraca:
+#     True jezeli odpowiedzi poprawna
+#     False jezeli odpowiedz bledna
+#  uruchom funkcje w petli 10 razy
+
+
+# Phase 3:
+#  wykorzystaj 2 zmienne:
+#  ilosc_poprawne - zawiera ilosc poprawnych odpowidzi. zwieksz za kazdym razem kiedy odpowiedz poprawan
+#  ilosc_pytan    - zawiera ilosc zadanych pytan.
+#  po zebraniou odpowiedzi wypisz na ekranie przy pomocy print
+#  "udzieloneo 2 poprawnych odpowiedzi na 10 pytan: 2/10"
+
+import random
+
+
+
+
+
+
+
+
+
+
