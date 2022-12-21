77065149e087d2be3fd3b1ae4137820a79d739a2
ernkoby5i <ernkoby5i@gmail.com>
2022-12-21 20:45:58 +0100

    angielski dodano statystyki

--- 11b_zadanie_4_nauka_angielskiego.py
+++ 11b_zadanie_4_nauka_angielskiego.py
@@ -9,7 +9,7 @@ POWITANIE = """
 -                                           -
 -  witam w programie do nauki angielskiego  -
 -                                           -
--  by Erni v.1.0.0            2022-12-03    -
+-  by Erni v.1.1.0            2022-12-21    -
 -                                           -
 ---------------------------------------------
 """
@@ -17,7 +17,7 @@ POWITANIE = """
 POZEGNANIE ="""
 ---------------------------------------------
 -                   Koniec                  -
--  by Erni v.1.0.0            2022-12-03    -
+-  by Erni v.1.1.0            2022-12-21    -
 ---------------------------------------------
 """
 
@@ -49,8 +49,13 @@ LENA_CHAPTER_1 = [("pada snieg", "snowing"), ("pada descz", "raining"),
 
 LISTA_ERNESTA = [("wiosna", "spring"), ("lato", "summer"), ("jesień", "autumn"), ("zima", "winter")]
 
+zadania_count = 10
+poprawne = 0
+blendne = 0
 
 def sprawdz_ucznia(SLOWA_LISTA):
+    global blendne
+    global poprawne
     ilosc_elementow=len(SLOWA_LISTA)
     x=random.randint(0, ilosc_elementow -1)
     element=SLOWA_LISTA[x]
@@ -60,8 +65,10 @@ def sprawdz_ucznia(SLOWA_LISTA):
     odpowiedz = input(f"jak jest po angielsku '{wyraz_pl}' ? : ")
     if (wyraz_en==odpowiedz):
         print("BRAWO!:)")
+        poprawne = poprawne + 1
     else:
         print(f"poprawna odpowiedz to: ", wyraz_en)
+        blendne = blendne + 1
     #print(x, SLOWA_LISTA[x], element, wyraz_pl, wyraz_en, odpowiedz)
 
 
@@ -76,16 +83,25 @@ if (imie == "Lena"):
 else:
     lista = LISTA_ERNESTA
 
-for i in range(5):
+for i in range(zadania_count):
     print("")
     print(f"zadanie {i+1}:")
     sprawdz_ucznia(lista)
 
+
+
+print("")
+print("Odpowiedzi złe   : ",blendne)
+print("Odpowiedzi dobre : ",poprawne)
+print(f"Twoj wynik to    : {poprawne}/{zadania_count}")
+print("")
 print(POZEGNANIE)
 print(f"pa pa, {imie}.")
 print(f"Have a nice day.")
 
 input("press enter")
 
-
-
+#if zadania_count == 10:
+#questions_count += 1
+#poprawne = poprawne+1
+#blendne = blendne+1
