6ce1815b541df1ef8666bb19d75918a440e88a66
ernkoby5i <ernkoby5i@gmail.com>
2022-12-28 01:29:57 +0100

    05_zimowe_wyzwanie/Cukierki_10

--- instalogik/05_zimowe_wyzwanie/Cukierki_08 20 paczek po 1-9 wybrac 4 paczki.py
+++ instalogik/05_zimowe_wyzwanie/Cukierki_08 20 paczek po 1-9 wybrac 4 paczki.py
@@ -1,5 +1,6 @@
 """
-Program 8. W cukierni jest 20 paczek cukierków, a w każdej z nich jest od 1 do 9 cukierków.
+Program 8.
+W cukierni jest 20 paczek cukierków, a w każdej z nich jest od 1 do 9 cukierków.
 Napisz program, który po podaniu liczby cukierków w każdej z paczek,
 obliczy ile najwięcej cukierków można otrzymać wybierając 4 paczki.
 """
