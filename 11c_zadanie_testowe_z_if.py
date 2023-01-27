8f8ee4bdf6240980a3ace68e8d22c1a4b87cf2a3
ernkoby5i <ernkoby5i@gmail.com>
2023-01-27 23:25:41 +0100

    zadanie testowe

---
+++ 11c_zadanie_testowe_z_if.py
@@ -0,0 +1,14 @@
+D = int(input("wpisz liczbe "))
+C = D
+while (C>0):
+    C = C-1
+    print(D)
+
+print("KONIEC CĘŚCI 1")
+
+for y in range(1, D + 1, 1):
+
+    if(y % 2 == 1):
+        print("niemazyste",y)
+
+
