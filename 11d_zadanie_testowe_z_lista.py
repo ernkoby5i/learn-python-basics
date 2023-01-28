0ba729e4246f82c01f6a84b4c3cdb3b2a05e3d6f
ernkoby5i <ernkoby5i@gmail.com>
2023-01-28 13:00:34 +0100

    zadania instakod

---
+++ 11d_zadanie_testowe_z_lista.py
@@ -0,0 +1,5 @@
+lista = ["jabułko ", "truskawka ", "pomarańcza ", "cebula ", "marchewka"]
+i = 0
+while i < len(lista):
+    print(lista[i])
+    i += 1
---
+++ Zadanie Py 1.8.py
@@ -0,0 +1,12 @@
+s = """Grupa #1 Wzrost uczniów
+
+1. Adam   154 cm
+2. Kasia   162 cm
+3. Franek   167 cm"""
+print(s)
+
+print("Grupa #1 Wzrost uczniów")
+print()
+print("1. Adam   154 cm")
+print("2. Kasia   162 cm")
+print("3. Franek   167 cm")
